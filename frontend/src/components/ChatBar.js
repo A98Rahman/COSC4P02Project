b232cb5a471ff87c3632c237064ae51c7d38b4f2
raykeating <raykeating13@gmail.com>
2022-02-12 14:08:19 -0500

    Delete frontend directory

--- frontend/src/components/ChatBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import colorScheme from "../colorScheme"
-
-export default function ChatBar() {
-	return (
-		<div style={{ flex: "0 0 40px", margin: "8px 8px 8px 8px", background: "white"}}>
-		</div>
-	)
-}
