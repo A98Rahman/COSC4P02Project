18513dde591f44bb2d868b1b0ffad95e2ef0bee3
Ray-Ubuntu <raykeating13@gmail.com>
2022-02-15 12:00:34 -0500

    Added basic example DB query to rasa bot

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -6,23 +6,30 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from pymongo import MongoClient
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# class ActionHelloWorld(Action):
+class ActionCourseInfo(Action):
 
-#     def name(self) -> Text:
-#         return "action_hello_world"
+    def name(self) -> Text:
+        return "action_course_info"
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-#         dispatcher.utter_message(text="Hello World!")
+        client = MongoClient('mongodb+srv://RasaApp:Chatbot123@cluster0.phnw6.mongodb.net/myFirstDatabase?retryWrites=true')
+        db = client["BrockInfo"] # db holds "BrockInfo" database
+        courses = db.get_collection("Courses")
+        course = courses.find_one({"course_code": "COSC3P03"})
 
-#         return []
+        # response
+        if course:
+            dispatcher.utter_message(text=f'{course["course_name"]} is taught by {course["instructor"]}')
+        else:
+            dispatcher.utter_message(text="I couldn't find that course")
+
+        return []
