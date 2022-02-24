872b2a3f597db468afa94b4e0edec412c5002491
Austin Goodman <ag17za@brocku.ca>
2022-02-24 09:12:53 -0500

    Added non functional submit button to the chat bar.

--- frontend/src/components/ChatBar.js
+++ frontend/src/components/ChatBar.js
@@ -1,4 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import useStateRef from '../useStateRef'
 import colorScheme from "../colorScheme"
 import FlexContainer from './FlexContainer'
@@ -45,9 +47,20 @@ export default function ChatBar({ onSubmitMessage }) {
 	}
 
 	return (
-		<FlexContainer style={{ flex: "0 0 40px", margin: "8px 8px 8px 8px" }}>
-			<form onSubmit={handleOnSubmit} style={{ width: "100%", display: "flex" }}>
-				<input onInput={handleOnInput} onBlur={handleOnBlur} type="text" placeholder="Ask a question" style={{ width: "100%", height: "40px" }}></input>
+		<FlexContainer style={{ flex: "0 0 40px", margin: "8px 8px 8px 8px", background: "white" }}>
+			<form onSubmit={handleOnSubmit} style={{ width: "100%", display: "flex", overflow: "hidden", borderRadius: "4px", boxShadow: "0px 2px 2px 1px rgba(0, 0, 0, 0.1)" }}>
+				<input
+					onInput={handleOnInput}
+					onBlur={handleOnBlur}
+					type="text"
+					placeholder="Ask a question"
+					style={{ width: "100%", height: "40px", border: "none" }}
+				/>
+				<div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: "0 0 auto", background: colorScheme.darkRed }}>
+					<div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: "0 0 auto", margin: "0px 8px 0px 8px" }}>
+						<FontAwesomeIcon icon={solid('paper-plane')} size="1x" style={{ height: "24px", color: "white" }} />
+					</div>
+				</div>
 			</form>
 
 		</FlexContainer>
