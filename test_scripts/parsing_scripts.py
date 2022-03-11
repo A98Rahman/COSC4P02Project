0763f1090cdc26a2ee5b4a20593d3f56967765ea
Ray-Ubuntu <raykeating13@gmail.com>
2022-03-11 15:16:14 -0500

    added basic club lookup functionality to rasa bot

--- test_scripts/parsing_scripts.py
+++ test_scripts/parsing_scripts.py
@@ -1,13 +1,15 @@
 import json
 
+from itsdangerous import exc
+
 def parse_JSON_to_YML():
-    f = open('CourseInfo.json')
-    w = open('CourseLookup.txt', 'w')
+    f = open('ClubInfo.json')
+    w = open('ClubLookup.txt', 'w')
 
     data = json.load(f)
 
     for i in data:
-        w.write("- "+ i["course_name"]+ "\n")
+        w.write("- "+ i["club_name"]+ "\n")
 
     f.close()
 
@@ -19,6 +21,39 @@ def remove_duplicates():
             outfile.write(line)
             lines_seen.add(line)
     outfile.close()
+
+def parse_clubs_text_data():
+    f = open('clubs.txt', 'r')
+    w = open('ClubInfo.json', 'w')
+
+    w.write("[{")
+
+    counter = 0
+
+    for i in f:
+        try:
+            if "/-/" in i:
+                nextbracket = "{"
+                w.write(nextbracket)
+            elif i[5] == "N":
+                club_name = i[11:].strip()
+                club_name = club_name.replace("'", "")
+                w.write(f'\"club_name\": \"{club_name}\",')
+            elif i[5] == "D":
+                club_description = i[18:].strip()
+                w.write(f'\"club_description\": \"{club_description}\"')
+                nextbracket = "},\n"
+                w.write(nextbracket)
+            
+        except:
+            print('error')
+        counter += 1
+
+    w.write("]")
+
+    w.close()
+    f.close()
+
 
 def main():
     parse_JSON_to_YML()
