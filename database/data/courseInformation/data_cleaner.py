4ccda242b42046db4c413a73a61e7ec83632f9fe
A98Rahman <63158648+A98Rahman@users.noreply.github.com>
2022-04-05 14:52:40 -0400

    Database changes
    Merged faculty and staff tables. now all the data is held in the staff table.
    
    Entered all the course information in the database

---
+++ database/data/courseInformation/data_cleaner.py
@@ -0,0 +1,33 @@
+import json
+import glob
+
+allfiles = glob.glob('*.json')
+
+print(allfiles)
+
+cleanedData = []
+for file in allfiles:
+    j = json.loads(open(file).read())
+    isnull = False
+    for rec in j:
+        print(rec)
+        # print(cd'\n')
+        # print(type(rec))
+        # for entry in rec:
+            # print(entry)
+            # if rec[entry] == None :
+            #     print(rec)
+            #     isnull = True
+            #     break
+        
+        # if(isnull):
+        #     print("Record is null")
+        #     isnull = False
+        # else:
+        cleanedData.append(rec)
+
+
+output = open('sanitizedData.json','w')    
+json.dump(cleanedData,output)
+# output.write(str(cleanedData))
+
