559e1fe7e68454f6a5a647d31600cb39ba401141
Austin Goodman <ag17za@brocku.ca>
2022-04-06 10:42:45 -0400

    Changed font. Added quicklinks to landing page header.

--- frontend/src/components/landing-page/Header.js
+++ frontend/src/components/landing-page/Header.js
@@ -8,10 +8,13 @@ export default function Header() {
 
 	return (
 		<>
-			<div style={{ position: "absolute", width: "100%", height: "30vh", background: theme.colors.primaryColor }}>
-
-			</div>
-			<FlexContainer justifyContent="center" style={{ width: "100%", height: "40vh", padding: "24px", boxSizing: "border-box", zIndex: 1 }}> {/* header */}
+			<FlexContainer justifyContent="space-between" style={{ position: "absolute", width: "100%", height: "30vh", gap: "30px", padding: "8px 16px 0px 16px", boxSizing: "border-box", background: theme.colors.primaryColor }}>
+				<a href="https://brocku.ca/" style={{ height: "min-content", fontSize: `${1.2 * theme.fontScaleFactor}rem`, color: "white" }}>Brock University</a>
+				<a href="https://brocku.ca/brock-news/" style={{ height: "min-content", fontSize: `${1.2 * theme.fontScaleFactor}rem`, color: "white" }}>The Brock News</a>
+				<a href="https://www.stcatharines.ca/en/index.aspx" style={{ height: "min-content", fontSize: `${1.2 * theme.fontScaleFactor}rem`, color: "white" }}>City of St. Catharines</a>
+				<a href="" style={{ height: "min-content", fontSize: `${1.2 * theme.fontScaleFactor}rem`, color: "white" }}>Development Team</a>
+			</FlexContainer>
+			<FlexContainer justifyContent="center" style={{ width: "100%", height: "40vh", padding: "40px", boxSizing: "border-box", zIndex: 1, pointerEvents: "none" }}> {/* header */}
 				<img src={BadgerImage1} alt="Badger Logo" style={{ position: "relative", marginRight: "32px" }} />
 				<h1 style={{ fontSize: "64px", alignSelf: "center", color: "#ededed", marginBottom: "10vh" }}>Brock University <br></br> Chatbadger</h1>
 			</FlexContainer>
